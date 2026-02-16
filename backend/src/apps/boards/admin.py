19d097d47543de721db3da7277541b647a868bb8
Tiago-Monteirox <tiagomonteirodev@outlook.com>
2026-02-15 21:21:32 -0300

    Community implementation

--- backend/src/apps/boards/admin.py
+++ backend/src/apps/boards/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+<<<<<<< HEAD
 from .models import Skill, UserSkill
 
 
@@ -15,3 +16,6 @@ class UserSkillAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "skill", "level", "years_exp", "can_mentor")
     list_filter = ("can_mentor", "level", "skill__category")
     search_fields = ("user__email", "skill__name")
+=======
+# Register your models here.
+>>>>>>> 13000a6 (chore: setup inicial (Django/Ninja, settings por env, docker-compose Postgres))
